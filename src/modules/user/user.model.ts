c1211580dfa8f9de0acc74fe6eea385e90985554
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-07-15 14:40:50 +0600

    added user model

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+import { IUser } from './user.interface';
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+
+
+export const User = model('User', userSchema);
