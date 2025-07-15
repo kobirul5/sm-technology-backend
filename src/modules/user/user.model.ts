4cad79320c73d0356dd7711bf085b7e7c0bb84eb
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-07-15 14:51:45 +0600

    create mongoose model for check password, generate token and bycript passoword

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,4 +1,6 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
 import { IUser } from './user.interface';
 
 const userSchema = new Schema<IUser>(
@@ -13,6 +15,26 @@ const userSchema = new Schema<IUser>(
   }
 );
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
+userSchema.methods.isPasswordCorrect = async function (password: string) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      role: this.role
+    },
+    process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '2d' }
+  )
+} 
 
 export const User = model('User', userSchema);
